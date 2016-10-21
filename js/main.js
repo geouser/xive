4f901e9e107471aa592df7a60e2d7497ca052cdc
Serhiy Hulyi <serhiyhulyi93@gmail.com>
2016-10-21 16:20:49 +0300

    start

---
+++ js/main.js
@@ -0,0 +1,121 @@
+// Global parameters
+window.params = {
+    isMobile: /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),
+    isIOS: /iPhone|iPad|iPod/i.test(navigator.userAgent)
+};
+
+
+jQuery(document).ready(function($) {
+
+    /*---------------------------
+                                  ADD CLASS ON SCROLL
+    ---------------------------*/
+    $(function() { 
+        var $document = $(document),
+            $element = $('.menu-button'),
+            $element2 = $('header'),
+            className = 'hasScrolled';
+
+        $document.scroll(function() {
+            $element.toggleClass(className, $document.scrollTop() >= 1);
+            $element2.toggleClass(className, $document.scrollTop() >= 1);
+        });
+    });
+      
+
+    /*---------------------------
+                                  MENU TOGGLE
+    ---------------------------*/
+    $('.menu-button').on('click', function(event) {
+        event.preventDefault();
+        $(this).toggleClass('active');
+        $(this).siblings('header').toggleClass('active');
+        if ($('header').hasClass('active')) {
+                $('body, html').css('overflow', 'hidden');
+            } else {
+                $('body, html').css('overflow', 'visible');
+            }
+    });
+
+
+
+    /*---------------------------
+                                  Magnific popup
+    ---------------------------*/
+    $('.magnific').magnificPopup({
+        type: 'inline',
+
+        fixedContentPos: false,
+        fixedBgPos: true,
+
+        overflowY: 'auto',
+        modal: false,
+
+        closeBtnInside: true,
+        preloader: false,
+        
+        midClick: true,
+        removalDelay: 300,
+        mainClass: 'my-mfp-slide-bottom'
+    });
+
+
+
+    /*----------------------------
+                              SEND FORM
+    -------------------------*/
+    /**
+     *
+     * Open popup
+     *
+     * @param popup {String} jQuery object (#popup)
+     *
+     * @return n/a
+     *
+    */
+    function openPopup(popup){
+        $.magnificPopup.open({
+            items: {
+              src: popup
+            },
+            type: 'inline',
+            fixedContentPos: false,
+            fixedBgPos: true,
+            overflowY: 'auto',
+            modal: false,
+            closeBtnInside: true,
+            preloader: false,
+            midClick: true,
+            removalDelay: 300,
+            mainClass: 'my-mfp-slide-bottom'
+        }, 0);
+    }
+
+    $('form').on('submit', function(event) {
+        event.preventDefault();
+        /* Act on the event */
+        var data = $(this).serialize();
+        $.ajax({
+            url: theme.url + '/forms.php',
+            type: 'POST',
+            data: data,
+            success: function(result){
+                if (result.status == 'ok') {
+                    openPopup('#modal-popup-ok')
+                } else {
+                    openPopup('#modal-popup-error')
+                }
+            },
+            error: function(result) {
+                openPopup('#modal-popup-error');
+            }
+        })
+        .always(function() {
+            $('form').each(function(index, el) {
+                $(this)[0].reset();
+            });
+        });
+        
+    });
+
+}); // end file
